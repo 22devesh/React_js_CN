e13c8042bee32197b2b389a4ac4504d5e6baa81d
Devesh <singhaldevesh514@gmail.com>
2025-05-21 10:55:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+/** JavaScript */
+// const heading = document.createElement("h2");
+// heading.textContent="Hello World";
+// heading.className = "header";
+// document.getElementById("root").append(heading);
+
+// console.log("JavaScript element: ",heading)
+
+/** REACT With JS*/
+
+//  const reactHeading =React.createElement("h1",{className : "head",id:"reactHead", children:"HelloReact!"});
+
+/** REACT With JSX */
+
+const jsxHeading= 
+
+
+ ReactDOM.createRoot(document.getElementById("root")).render(jsxHeading);
+
+//  console.log("JavaScript element: ",reactHeading);
+ 
+
